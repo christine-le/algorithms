b6421b3c31a6632dfdfc36754e5f6635246e743c
christine <christinele.cs@gmail.com>
2018-01-29 21:26:34 -0800

    Added recursive palindrome exercise

--- recursion.py
+++ recursion.py
@@ -8,3 +8,18 @@ def print_array(arr):
     
   
 print_array([1,2,3,4])
+
+
+# Recursively check if word is a palindrome
+def is_palindrome(word):
+  if len(word) == 0:
+    return True
+
+  else:
+    if word[0] == word[len(word)-1]:
+      word = word[1:len(word)-1]
+      return is_palindrome(word)
+    else:
+      return False
+  
+print is_palindrome('tacocat')
