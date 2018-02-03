6d1f212ed3483d6fc0d522a2db3b95bf532c7aa3
Christine Le <christinele.cs@gmail.com>
2018-02-03 13:41:06 -0800

    add insertion sort

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+# Time complexity: O(n^2)
 def selection_sort(arr):
     for i in range(0, len(arr)):
         minIdx = i
@@ -10,17 +11,21 @@ def selection_sort(arr):
         arr[minIdx] = tmp
     return arr
 
+print "selection sort: ", str(selection_sort([7,8,5,4,9,2]))
 
-print selection_sort([7,8,5,4,9,2])
-
-
+# Time Complexity: O(n^2)
 def insertion_sort(arr):
-  for i in range(1, len(arr)):
-    for curr in range(i, 0, -1):
-      if arr[curr] < arr[curr-1]:
-        arr.insert(curr-1, arr.pop(curr))
-    
-  return arr
+    for i in range(1, len(arr)):
+        curr = i
+        val = arr[i]
+
+        while val < arr[curr-1] and curr > 0:
+            arr[curr] = arr[curr-1]
+            curr -= 1
+
+        arr[curr] = val
+
+    return arr
   
-print insertion_sort([3,2,6,1])
+print "insertion sort:", str(insertion_sort([3,2,6,1]))
 
