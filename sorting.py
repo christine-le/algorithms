49d1a14e3e8c509426eb3620717f4ce149cc0677
christine <christinele.cs@gmail.com>
2018-02-03 21:45:10 -0800

    solved merge sort exercise

--- sorting.py
+++ sorting.py
@@ -1,3 +1,6 @@
+# Algorithm:  Repeatedly finds the min element in the unsorted portion of the array
+# and swaps with the last unsorted element in array
+# Time complexity: O(n^2)
 def selection_sort(arr):
     for i in range(0, len(arr)):
         minIdx = i
@@ -10,17 +13,54 @@ def selection_sort(arr):
         arr[minIdx] = tmp
     return arr
 
+print "selection sort: ", str(selection_sort([7,8,5,4,9,2]))
 
-print selection_sort([7,8,5,4,9,2])
-
-
+# Algorithm:  Iterate through aray and insert current element from the unordered portion 
+# of the array into the ordered portion of the array
+# Time Complexity: O(n^2)
 def insertion_sort(arr):
-  for i in range(1, len(arr)):
-    for curr in range(i, 0, -1):
-      if arr[curr] < arr[curr-1]:
-        arr.insert(curr-1, arr.pop(curr))
-    
-  return arr
-  
-print insertion_sort([3,2,6,1])
+    for i in range(1, len(arr)):
+        curr = i
+        val = arr[i]
 
+        while val < arr[curr-1] and curr > 0:
+            arr[curr] = arr[curr-1]
+            curr -= 1
+
+        arr[curr] = val
+
+    return arr
+  
+print "insertion sort: ", str(insertion_sort([3,2,6,1]))
+
+# Algorithm:  Use a divide and conquer method to continually divide each half of array
+# and sort.  Merge each sorted sub-arrays with each iteration.
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+
+    new_array = []
+    half = len(arr)/2
+    arr1 = merge_sort(arr[0:half])    # Continuously divides left half and sort
+    arr2 = merge_sort(arr[half:])     # Continuously divides right half and sort
+
+    new_array = []
+    i =  j = 0
+    while i < len(arr1) or j < len(arr2):   # Sort and merge subarrays
+        if arr1[i] < arr2[j]:
+            new_array.append(arr1[i])
+            i += 1
+        else:
+            new_array.append(arr2[j])
+            j += 1
+
+        if i == len(arr1):
+            new_array = new_array + arr2[j:]
+            break
+        if j == len(arr2):
+            new_array = new_array + arr1[i:]
+            break
+
+    return new_array
+    
+print "merge sort: ", str(merge_sort([7,8,5,4,9,2,1]))
